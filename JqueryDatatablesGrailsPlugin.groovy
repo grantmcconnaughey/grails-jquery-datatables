6463910f8788ae3ce20fc0965e8f7649e35306f2
Grant McConnaughey <mcconnaughey-g@mssu.edu>
2014-12-08 09:15:43 -0600

    Update plugin descriptor

--- JqueryDatatablesGrailsPlugin.groovy
+++ JqueryDatatablesGrailsPlugin.groovy
@@ -1,17 +1,16 @@
 class JqueryDatatablesGrailsPlugin {
     def version = "1.10.4"
     def grailsVersion = "2.0 > *"
-    def pluginExcludes = [ ]
-
-    def author = "Lim Chee Kin"
-    def authorEmail = "limcheekin@vobject.com"
     def title = "JQuery DataTables Plugin"
+    def author = "Grant McConnaughey"
+    def authorEmail = "grantmcconnaughey@gmail.com"
+    def developers = [ [ name: "Lim Chee Kin", email: "limcheekin@vobject.com" ]]
     def description = '''\
 This plugin simply supplies jQuery DataTables plugin resources, and depends on the jQuery plugin to include the core jquery libraries.
 Use this plugin in your own apps and plugins to avoid resource duplication and conflicts.
 '''
-
-    // URL to the plugin's documentation
-    def documentation = "http://grails.org/plugin/jquery-datatables"
-
+    def documentation = "https://github.com/grantmcconnaughey/grails-jquery-datatables"
+    def license = "APACHE"
+    def issueManagement = [ system: "Github", url: "https://github.com/grantmcconnaughey/grails-jquery-datatables/issues" ]
+    def scm = [ url: "https://github.com/grantmcconnaughey/grails-jquery-datatables" ]
 }
